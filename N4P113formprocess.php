67ae1ce82d885ff4a2d96c6c1003db9998763f16
daw2-ibanez22 <91276106+daw2-ibanez22@users.noreply.github.com>
2021-11-05 17:53:15 +0100

    Add files via upload

---
+++ N4P113formprocess.php
@@ -0,0 +1,23 @@
+<html>
+ <head>
+  <title>Multipurpose Form</title>
+ </head>
+ <body>
+  <form action="#" method="post">
+   <table>
+     <tr>
+     <td>Eligue la opcion que quieres:<br/></td>
+     <td>
+      <select name="hola:profe">
+       <option value="#"> <?php echo $_POST['name1']; ?></option>
+       <option value="#"><?php echo $_POST['name2']; ?></option>
+       <option value="#"><?php echo $_POST['name3']; ?></option>
+       <option value="#"><?php echo $_POST['name4']; ?></option>
+       <option value="#"><?php echo $_POST['name5']; ?></option>
+      </select>
+     </td>
+    </tr>
+   </table>
+  </form>
+ </body>
+</html>
